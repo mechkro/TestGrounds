012c64e5e54053617c754591ddf18dc6f35ca453
mechkro <kinsey30@yahoo.com>
2020-03-04 08:05:05 -0700

    Add files via upload
    Files Working Together
    
    - oldwin
    - test3
    - test2

---
+++ oldwin.py
@@ -0,0 +1,67 @@
+import tkinter as tk
+from test2 import newwin2
+
+
+
+
+class Main(object):
+    """ """
+    def __init__(self, parent):
+        self.parent = parent
+        self.f = tk.Frame(self.parent, bg = 'red')
+        self.f.grid(padx = 2, pady = 2)
+
+        self.l = tk.Button(self.f, text = 'Win 1',
+                           command = lambda: self.newwin())
+        self.l.grid(padx = 10, pady = 10)
+
+    #--------------------------------------
+    def newwin(self):
+        """ """
+        self.nw = newwin2(self.parent)
+
+
+
+
+class Recalled(object):
+    """If this file wants to be called back, created this to mimic
+    the other files which have been called prior.
+    I feel this is incorrect way of performing thiscallback but my
+    lack of deep understanding of OOP is the cause
+    """
+    
+    def __init__(self, master):
+        self.master = master
+        self.emptycurrent()
+        self.addnew()
+
+    #--------------------------------------
+    def emptycurrent(self):
+        """ """
+        for i in self.master.winfo_children():
+            i.destroy()
+        return
+
+    #--------------------------------------
+    def addnew(self):
+        """ """
+        self.f = tk.Frame(self.master, bg = 'red')
+        self.f.grid(padx = 2, pady = 2)
+
+        self.l = tk.Button(self.f, text = 'Win 1 is back!',
+                           command = lambda: self.newwin())
+        self.l.grid(padx = 10, pady = 10)
+
+    #--------------------------------------
+    def newwin(self):
+        """ """
+        self.nw = newwin2(self.master)
+
+    
+
+
+#--------------------------------------
+if __name__ == '__main__':
+    root = tk.Tk()
+    Main(root)
+    root.mainloop()
---
+++ test2.py
@@ -0,0 +1,30 @@
+import tkinter as tk
+from test3 import newwin3
+
+
+class newwin2(object):
+    """ """
+    
+    def __init__(self, master):
+        self.master = master
+        self.emptycurrent()
+        self.addnew()
+
+    #--------------------------------------
+    def emptycurrent(self):
+        for i in self.master.winfo_children():
+            i.destroy()
+        return
+
+    #--------------------------------------
+    def addnew(self):
+        self.f = tk.Frame(self.master, bg = 'blue')
+        self.f.grid(padx = 2, pady = 2)
+
+        self.l = tk.Button(self.f, text = 'Win 2',
+                           command = lambda: self.newwin())
+        self.l.grid(padx = 10, pady = 10)
+
+    #--------------------------------------
+    def newwin(self):
+        self.nw = newwin3(self.master)
