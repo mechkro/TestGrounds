d857a3b9b7b7a8f65c54a919ce28ab43f265324a
mechkro <kinsey30@yahoo.com>
2020-03-04 21:28:00 -0700

    Update test3.py

--- MultipleWindows/test3.py
+++ MultipleWindows/test3.py
@@ -1,40 +1,44 @@
 import tkinter as tk
 
 
+BG = '#0C1021'
+FG = 'white'
+
+
 
 class newwin3(object):
     """ """
     
-    def __init__(self, master):
+    def __init__(self, master, frm):
         self.master = master
+        self.frm = frm
         self.emptycurrent()
         self.addnew()
 
     #--------------------------------------
     def emptycurrent(self):
         """ """
-        for i in self.master.winfo_children():
+        for i in self.frm.winfo_children(): #master.winfo_children():
             i.destroy()
         return
 
     #--------------------------------------
     def addnew(self):
         """ """
-        self.f = tk.Frame(self.master, bg = 'green')
+        self.f = tk.Frame(self.frm, bg = BG)
         self.f.grid(padx = 2, pady = 2)
 
-        self.l = tk.Button(self.f, text = 'Win 3',
+        self.l = tk.Button(self.f, text = 'Win 3', bg = BG, fg = FG,
                            command = lambda: self.newwin())
         self.l.grid(padx = 10, pady = 10)
 
     #--------------------------------------
     def newwin(self):
         """ """
-        self.ll = tk.Label(self.f, text = 'Tada',
-                           bg = 'green')
+        self.ll = tk.Label(self.f, text = 'Tada', bg = BG, fg = FG)
         self.ll.grid(row = 1, padx = 5, pady = 5)
 
-        self.b2 = tk.Button(self.f, text = 'back',
+        self.b2 = tk.Button(self.f, text = 'back', bg = BG, fg = FG,
                             command = lambda: self.backwin())
         self.b2.grid(row = 2, padx = 5, pady = 5)
 
@@ -42,4 +46,47 @@ class newwin3(object):
     def backwin(self):
         """ """
         from oldwin import Recalled
-        self.bw = Recalled(self.master)
+        self.bw = Recalled(self.master, self.frm)
+
+
+
+
+
+#-------------------------------------------
+class Recalled(object):
+    """If this file wants to be called back, created this to mimic
+    the other files which have been called prior.
+    I feel this is incorrect way of performing thiscallback but my
+    lack of deep understanding of OOP is the cause
+    """
+    
+    def __init__(self, master, frm):
+        self.master = master
+        self.frm = frm
+        self.emptycurrent()
+        self.addnew()
+
+    #--------------------------------------
+    def emptycurrent(self):
+        """ """
+        
+        for i in self.frm.winfo_children():  #master.winfo_children():
+            i.destroy()
+        return
+
+    #--------------------------------------
+    def addnew(self):
+        """ """
+        
+        self.f = tk.Frame(self.frm, bg = BG)
+        self.f.grid(padx = 2, pady = 2)
+
+        self.l = tk.Button(self.f, text = 'Win 1 is back!', bg = BG, fg = FG,
+                           command = lambda: self.newwin())
+        self.l.grid(padx = 10, pady = 10)
+
+    #--------------------------------------
+    def newwin(self):
+        """ """
+        
+        self.nw = newwin2(self.master,self.frm)
