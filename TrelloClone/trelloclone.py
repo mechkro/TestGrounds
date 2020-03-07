c7e51441fa2ebb19ac9183c111e2b9eb353a7d3a
mechkro <kinsey30@yahoo.com>
2020-03-06 21:31:57 -0700

    Update trelloclone.py
    Added more structure and widgets
    
    *** Need to add more comments

--- TrelloClone/trelloclone.py
+++ TrelloClone/trelloclone.py
@@ -17,32 +17,62 @@ class Initiate(object):
     
     #-----------------------------------------------------------------------------------
     def __init__(self, master):
+        """ 
+        
+        """
         
         self.master = master
+        self.master.title('Trello Clone')
         self.master.config(bg = BG)
         
         self.framehold = {}
         self.labelhold = {}
         self.labelframehold = {}
+        self.texthold = {} 
+        
+        col_titles = ['Not Started', 'In Progress', 'Completed']
+        col_desc = ['items that have not been looked into', 'Click each line for entry details', 'Items that have been completed']
         itr = 0
         
         for i in range(3):
             for j in range(3):
                 
-                self.framehold[itr] = self.make_frame().grid(row = i, column = j, padx = 10, pady = 10)   #*[int(i), int(j)])
+                self.framehold[itr] = self.make_frame().grid(row = i, column = j, padx = 5, pady = 5)   #*[int(i), int(j)])
+                
                 if itr <= 2:
-                    self.labelframehold[j] = self.make_labelframe(self.framehold[itr], 'Frame {}'.format(itr), *[i, j])
-                    self.labelhold[itr] = self.make_label(self.labelframehold[itr], '{}'.format(itr), *[ i, j])
-                else:
-                    self.labelhold[itr] = self.make_label(self.framehold[itr], '{}'.format(itr), *[ i, j])
+                    self.labelframehold[j] = self.make_labelframe(self.framehold[itr], '{}'.format(col_titles[itr]), *[i, j])
+                    self.labelhold[itr] = self.make_label(self.labelframehold[itr], '{}'.format(col_desc[itr]), *[ i, j])
+                    
+                if 2 < itr <= 5:
+                    self.texthold[itr-3] = self.make_text(self.framehold[itr], *[i,j])
+                    txt = 'This is label frame {}'.format(itr-2)
+                    self.texthold[itr-3].insert(tk.END, txt)
+                    
+                if itr > 5:
+                    self.texthold[itr-3] = self.make_text(self.framehold[itr], *[i,j])
+                    txt = 'This is label frame {}'.format(itr-2)
+                    self.texthold[itr-3].insert(tk.END, txt)                    
+                    #self.labelhold[itr] = self.make_label(self.framehold[itr], '{}'.format(itr), *[ i, j])
                     
                 itr += 1
+        
+        
+        self.butt_fwd_col1 = tk.Button(self.master, text = '>', bg = BG, fg = FG, command = None, font = ('Vardana',14,'bold'))
+        self.butt_fwd_col1.grid(row = 4, column = 0, pady = 5, sticky = tk.EW)
+        
+        self.butt_fwd_col2 = tk.Button(self.master, text = '<    >', bg = BG, fg = FG, command = None, font = ('Vardana',14,'bold'))
+        self.butt_fwd_col2.grid(row = 4, column = 1, pady = 5, sticky = tk.EW)
+        
+        self.butt_fwd_col3 = tk.Button(self.master, text = '<', bg = BG, fg = FG, command = None, font = ('Vardana',14,'bold'))
+        self.butt_fwd_col3.grid(row = 4, column = 2, pady = 5, sticky = tk.EW)        
                 
                 
     
     #-----------------------------------------------------------------------------------
     def make_frame(self,*args):
-        """ """
+        """ 
+        
+        """
         #try:
             #f = tk.Frame(self.master, bg = BG)
             #f.grid(row = args[0], column = args[1], padx = 5, pady = 5)
@@ -51,14 +81,28 @@ class Initiate(object):
     
     #-----------------------------------------------------------------------------------
     def make_labelframe(self, parent, txt, *args):
-        """ """
+        """ 
+        
+        """
         #try:
             #f = tk.Frame(self.master, bg = BG)
             #f.grid(row = args[0], column = args[1], padx = 5, pady = 5)
-        lf = tk.LabelFrame(parent, text = txt, bg = BG, fg = FG, font = ('impact',18,'bold'))
-        lf.grid(row = args[0], column = args[1], padx = 12, pady = 12)
+            
+        lf = tk.LabelFrame(parent, text = txt, bg = BG, fg = FG, font = ('Vardana',18,'bold'))
+        lf.grid(row = args[0], column = args[1], padx = 5, pady = 5, sticky = tk.EW)
         return lf
     
+    #-----------------------------------------------------------------------------------
+    def make_text(self, parent, *args):
+        """ 
+        
+        """
+        #try:
+            #f = tk.Frame(self.master, bg = BG)
+            #f.grid(row = args[0], column = args[1], padx = 5, pady = 5)
+        t = tk.Text(parent, bg = BG, fg = FG, font = ('impact',8,'normal'), width = 37, height = 7)
+        t.grid(row = args[0], column = args[1], padx = 5, pady = 5)
+        return t
     
     #-----------------------------------------------------------------------------------
     def make_label(self, parent,  txt, *args):
@@ -67,8 +111,8 @@ class Initiate(object):
         During creation we call to binding function to add enter and leave functionality
         """
         
-        l = tk.Label(parent, text = txt, bg = BG, fg = FG, font = ('impact',18,'bold'))
-        l.grid(row = args[0], column = args[1], padx = 12, pady = 12)
+        l = tk.Label(parent, text = txt, bg = BG, fg = FG, font = ('Vardana',10,'normal'))
+        l.grid(row = args[0], column = args[1], padx = 5, pady = 5)
         self.add_binding(l)     #Add Binding to functionality before returning the label to dictionary
         
         return l   
@@ -76,7 +120,9 @@ class Initiate(object):
     
     #-----------------------------------------------------------------------------------
     def add_binding(self, widge):
-        """ """
+        """ 
+        
+        """
         
         widge.bind('<Enter>', self.change_txt_color)
         widge.bind('<Leave>', self.change_txt_def_color)
@@ -85,16 +131,20 @@ class Initiate(object):
     
     #-----------------------------------------------------------------------------------
     def change_txt_color(self, event):
-        """ """
+        """ 
         
-        event.widget.config(fg = DGR, font = ('impact',24,'bold'))
+        """
+        
+        event.widget.config(fg = DGR, font = ('Vardana',10,'normal'))
         return 
         
     #-----------------------------------------------------------------------------------
     def change_txt_def_color(self, event):
-        """ """
+        """ 
         
-        event.widget.config(fg = FG, font = ('impact',18,'bold'))
+        """
+        
+        event.widget.config(fg = FG, font = ('Vardana',10,'normal'))
         return         
         
     
