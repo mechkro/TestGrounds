bcac4dbd9a0ac1861407a4052826bd21affca5c7
mechkro <kinsey30@yahoo.com>
2020-03-08 20:27:32 -0700

    Update trelloclone.py
    New update that has most of the core functionality put in. Simple foux listbox entry function adds sample entries to be tried out with buttons.

--- TrelloClone/trelloclone.py
+++ TrelloClone/trelloclone.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import random
 
 
 #Configuration Variables -----------------------------------------------------
@@ -25,10 +26,12 @@ class Initiate(object):
         self.master.title('Trello Clone')
         self.master.config(bg = BG)
         
+        #Create the dictionariess to hold our widgets
         self.framehold = {}
         self.labelhold = {}
         self.labelframehold = {}
-        self.texthold = {} 
+        self.texthold = {}
+        self.listboxhold = {}
         
         col_titles = ['Not Started', 'In Progress', 'Completed']
         col_desc = ['items that have not been looked into', 'Click each line for entry details', 'Items that have been completed']
@@ -44,9 +47,10 @@ class Initiate(object):
                     self.labelhold[itr] = self.make_label(self.labelframehold[itr], '{}'.format(col_desc[itr]), *[ i, j])
                     
                 if 2 < itr <= 5:
-                    self.texthold[itr-3] = self.make_text(self.framehold[itr], *[i,j])
+                    #self.texthold[itr-3] = self.make_text(self.framehold[itr], *[i,j])
+                    self.listboxhold[itr -3] = self.make_listbox(self.framehold[itr], *[i,j])
                     txt = 'This is label frame {}'.format(itr-2)
-                    self.texthold[itr-3].insert(tk.END, txt)
+                    self.listboxhold[itr-3].insert(tk.END, txt)
                     
                 if itr > 5:
                     self.texthold[itr-3] = self.make_text(self.framehold[itr], *[i,j])
@@ -57,16 +61,47 @@ class Initiate(object):
                 itr += 1
         
         
-        self.butt_fwd_col1 = tk.Button(self.master, text = '>', bg = BG, fg = FG, command = None, font = ('Vardana',14,'bold'))
-        self.butt_fwd_col1.grid(row = 4, column = 0, pady = 5, sticky = tk.EW)
+        self.butt_fwd_col1 = tk.Button(self.master, text = '>', bg = BG, fg = FG, command = lambda: None, font = ('Vardana',14,'bold'))
+        self.butt_fwd_col1.grid(row = 4, rowspan = 2, column = 0, pady = 5, sticky = tk.EW)
         
-        self.butt_fwd_col2 = tk.Button(self.master, text = '<    >', bg = BG, fg = FG, command = None, font = ('Vardana',14,'bold'))
-        self.butt_fwd_col2.grid(row = 4, column = 1, pady = 5, sticky = tk.EW)
+        self.butt_fwd_col1.bind('<Button-1>', self.button_binding_1)
         
-        self.butt_fwd_col3 = tk.Button(self.master, text = '<', bg = BG, fg = FG, command = None, font = ('Vardana',14,'bold'))
-        self.butt_fwd_col3.grid(row = 4, column = 2, pady = 5, sticky = tk.EW)        
+        self.butt_fwd_col2_left = tk.Button(self.master, text = '<', bg = BG, fg = FG, command = lambda: None, font = ('Vardana',14,'bold'))
+        self.butt_fwd_col2_left.grid(row = 4, column = 1, pady = 5, sticky = tk.EW)
+        
+        self.butt_fwd_col2_left.bind('<Button-1>', self.button_binding_2_left)
+        
+        self.butt_fwd_col2_right = tk.Button(self.master, text = '>', bg = BG, fg = FG, command = lambda: None, font = ('Vardana',14,'bold'))
+        self.butt_fwd_col2_right.grid(row = 5, column = 1, pady = 5, sticky = tk.EW)
+        
+        self.butt_fwd_col2_right.bind('<Button-1>', self.button_binding_2_right)        
+        
+        self.butt_fwd_col3 = tk.Button(self.master, text = '<', bg = BG, fg = FG, command = lambda: None, font = ('Vardana',14,'bold'))
+        self.butt_fwd_col3.grid(row = 4, rowspan = 2, column = 2, pady = 5, sticky = tk.EW)
+        
+        self.butt_fwd_col3.bind('<Button-1>', self.button_binding_3)
+        
+        self.buttonhold = {1:self.butt_fwd_col1,
+                                      2:self.butt_fwd_col2_left,
+                                      3:self.butt_fwd_col2_right,
+                                      4:self.button_binding_3}
+        
+        
+        for i in range(50):
+            vals = list(self.listboxhold.values())
+            k = random.choice(vals)
+            self.testing_entries(k,i)
+
                 
-                
+    
+    #-----------------------------------------------------------------------------------
+    def testing_entries(self, widge, ent):
+        """ 
+        
+        """    
+        txt = 'This is label frame {}'.format(ent)
+        return widge.insert(tk.END, txt)
+        
     
     #-----------------------------------------------------------------------------------
     def make_frame(self,*args):
@@ -91,6 +126,20 @@ class Initiate(object):
         lf = tk.LabelFrame(parent, text = txt, bg = BG, fg = FG, font = ('Vardana',18,'bold'))
         lf.grid(row = args[0], column = args[1], padx = 5, pady = 5, sticky = tk.EW)
         return lf
+    
+    
+    #-----------------------------------------------------------------------------------
+    def make_listbox(self, parent, *args):
+        """ 
+        
+        """
+        #try:
+            #f = tk.Frame(self.master, bg = BG)
+            #f.grid(row = args[0], column = args[1], padx = 5, pady = 5)
+        lb = tk.Listbox(parent, bg = BG, fg = FG, font = ('impact',8,'normal'), width = 37, height = 12)
+        lb.grid(row = args[0], column = args[1], padx = 5, pady = 5)
+        return lb   
+    
     
     #-----------------------------------------------------------------------------------
     def make_text(self, parent, *args):
@@ -138,6 +187,7 @@ class Initiate(object):
         event.widget.config(fg = DGR, font = ('Vardana',10,'normal'))
         return 
         
+        
     #-----------------------------------------------------------------------------------
     def change_txt_def_color(self, event):
         """ 
@@ -147,12 +197,73 @@ class Initiate(object):
         event.widget.config(fg = FG, font = ('Vardana',10,'normal'))
         return         
         
-    
         
+    #-----------------------------------------------------------------------------------
+    def button_binding_1(self, event):
+        """ 
+        
+        """
+        
+        try:
+            select = self.listboxhold[0].get(tk.ACTIVE)
+            self.listboxhold[0].delete(tk.ACTIVE)
+            self.listboxhold[1].insert(tk.END, select) 
+            
+        except:
+            print('failed')
+     
+       
+    #-----------------------------------------------------------------------------------       
+    def button_binding_2_left(self, event):
+        """ 
+        
+        """
+        
+        try:
+            select = self.listboxhold[1].get(tk.ACTIVE)
+            self.listboxhold[1].delete(tk.ACTIVE)
+            self.listboxhold[0].insert(tk.END, select)        
+            
+        except:
+            print('failed')
+    
+    
+    #-----------------------------------------------------------------------------------       
+    def button_binding_2_right(self, event):
+        """ 
+        
+        """
+        
+        try:
+            select = self.listboxhold[1].get(tk.ACTIVE)
+            self.listboxhold[1].delete(tk.ACTIVE)
+            self.listboxhold[2].insert(tk.END, select)        
+            
+        except:
+            print('failed')
+            
+    
+    #-----------------------------------------------------------------------------------
+    def button_binding_3(self, event):
+        """ 
+        
+        """
+        
+        try:
+            select = self.listboxhold[2].get(tk.ACTIVE)
+            self.listboxhold[2].delete(tk.ACTIVE)
+            self.listboxhold[1].insert(tk.END, select)      
+            
+        except:
+            print('failed')
+        
+      
 
 
-
+#-----------------------------------------------------------------------------------
 if __name__ == '__main__':
+    """ """
+    
     root = tk.Tk()
     Initiate(root)
     root.mainloop()
