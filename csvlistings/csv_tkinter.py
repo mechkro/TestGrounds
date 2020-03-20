3e8b5b011ddb9189534940307031a61970f48990
mechkro <kinsey30@yahoo.com>
2020-03-19 22:10:55 -0700

    Update csv_tkinter.py

--- csvlistings/csv_tkinter.py
+++ csvlistings/csv_tkinter.py
@@ -1,4 +1,4 @@
-import tkinter as tk
+import Tkinter as tk
 
 
 
@@ -17,10 +17,16 @@ class Main(object):
                        (5,1),(5,2),(5,3),
                        (6,1),(6,2),(6,3)]
 
+        lf_hold = {}
         holdr = {}
-        for x,i in enumerate(grid_layout):
-            holdr[x] = self.make_lbox(i[-1], i[0])
 
+        for x,i in enumerate(grid_layout):
+            lf_hold[x] = self.make_lframe("Column {}".format(x),i[-1],i[0])
+            holdr[x] = self.make_lbox(lf_hold[x], 0, 0)
+
+        for v in lf_hold.values():
+            v.bind('<Button-1>', self.change_clr)
+            
         for v in holdr.values():
             v.bind('<Enter>', self.change_clr)
             v.bind('<Leave>', self.clr_def)
@@ -29,8 +35,13 @@ class Main(object):
         self.bot_txt.grid(row = 4, column = 0, columnspan = 7, padx = 3, pady = 3, sticky = tk.EW)
         
 
-    def make_lbox(self, r, c):
-        lb = tk.Listbox(self.f, bg = 'black', fg = 'white')
+    def make_lframe(self, txt, r, c):
+        lf = tk.LabelFrame(self.f, bg = 'black', fg = 'white', text = txt, font = ('Verdana',11,'bold'))
+        lf.grid(row = r, column = c, padx = 3, pady = 3)
+        return lf
+
+    def make_lbox(self, par, r, c):
+        lb = tk.Listbox(par, bg = 'black', fg = 'white')
         lb.grid(row = r, column = c, padx = 3, pady = 3)
         lb.insert(tk.END, 'Row = {}\nCol = {}'.format(r,c))
         return lb
@@ -56,4 +67,3 @@ if __name__ == '__main__':
     root = tk.Tk()
     Main(root)
     root.mainloop()
-
