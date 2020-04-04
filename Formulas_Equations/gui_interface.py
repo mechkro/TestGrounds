44a685b87b684180ba9126d0a2014fe699f4365a
mechkro <kinsey30@yahoo.com>
2020-04-03 18:27:33 -0700

    Update gui_interface.py
    added parameters list to iterate though and apply as label as well as add entry widget.

--- Formulas_Equations/gui_interface.py
+++ Formulas_Equations/gui_interface.py
@@ -2,15 +2,30 @@ import systemhead as syshead
 import tkinter as tk
 
 
+params_list = ('Flow rate (Q)(gpm)', 
+                        'Pipe Diam. (d)(in)',
+                        'Atm Pressure (P)(psi/ft)')
+                        
+
+
 
 class Main:
     
+    #-----------------------------------------------------------------------------
     def __init__(self, parent):
         """ """
         self.parent = parent
         self.f = tk.Frame(self.parent)
-        self.f.grid()
+        self.f.grid(column = 0)
         
+        self.fparams = tk.Frame(self.parent)
+        self.fparams.grid(column = 1)
+        
+        #Iterate through the parameters list to add them as labels along with entry widgets.
+        for n,i in enumerate(params_list):
+            tk.Label(self.fparams, text = i).grid(row = n, column = 0, padx = 3, pady = 3)
+            tk.Entry(self.fparams).grid(row = n, column = 1, padx = 3, pady = 3)
+            
         intro = """Welcome to the TDH Calculator interface.
 This tool will guide you through the calculation process
 for coming up with TDH, NPSHa, Suction Lift...etc"""
@@ -18,8 +33,9 @@ for coming up with TDH, NPSHa, Suction Lift...etc"""
         self.main_l = tk.Label(self.f, text = intro).grid(row = 0)
         self.main_butt = tk.Button(self.f, text = 'Begin', command = lambda: self.start())
         self.main_butt.grid(row = 1)
-        
-
+    
+    
+    #-----------------------------------------------------------------------------
     def start(self):
         """ """
         self.f.destroy()
@@ -38,8 +54,9 @@ for coming up with TDH, NPSHa, Suction Lift...etc"""
         
         self.butt2 = tk.Button(self.f2, text = 'Calculate', command = lambda: self.vel_head())
         self.butt2.grid(row = 3, column = 0, columnspan = 2, padx = 5, pady = 5)
+    
         
-        
+    #-----------------------------------------------------------------------------
     def vel_head(self):
         """ """
         e1 = float(self.ent1.get())
@@ -48,10 +65,12 @@ for coming up with TDH, NPSHa, Suction Lift...etc"""
         
         self.butt2.config(text = 'Next')
         self.l5 = tk.Label(self.f2, text = 'Velocity Head = {} ft.'.format(vhead)).grid(row = 4, column = 0, columnspan = 2,  padx = 5, pady = 5)
-        
 
+
+#-----------------------------------------------------------------------------
 if __name__ == '__main__':
     root = tk.Tk()
+    root.title('TDH Calculator')
     Main(root)
     root.mainloop()
 
