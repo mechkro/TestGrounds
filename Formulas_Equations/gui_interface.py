d656f6a11b927c77b09ae1af50c45d577de819dc
mechkro <kinsey30@yahoo.com>
2020-04-03 18:16:56 -0700

    Create gui_interface.py
    Start of the GUI file. Goal is to provide interface for non-command line functionality. 
    Issue is knowing what parameters the user has.
    
    Going forward - Maybe have all the parameters listed and allow the user to enter what they know. Then we can calculate
    what can be calculated and what parameters still need to be figured out or guesstimated on.

---
+++ Formulas_Equations/gui_interface.py
@@ -0,0 +1,57 @@
+import systemhead as syshead
+import tkinter as tk
+
+
+
+class Main:
+    
+    def __init__(self, parent):
+        """ """
+        self.parent = parent
+        self.f = tk.Frame(self.parent)
+        self.f.grid()
+        
+        intro = """Welcome to the TDH Calculator interface.
+This tool will guide you through the calculation process
+for coming up with TDH, NPSHa, Suction Lift...etc"""
+        
+        self.main_l = tk.Label(self.f, text = intro).grid(row = 0)
+        self.main_butt = tk.Button(self.f, text = 'Begin', command = lambda: self.start())
+        self.main_butt.grid(row = 1)
+        
+
+    def start(self):
+        """ """
+        self.f.destroy()
+        self.f2 = tk.Frame(self.parent)
+        self.f2.grid()
+        
+        self.l2 =tk.Label(self.f2, text = 'Velocity Head').grid(row = 0, column = 0, columnspan = 2, padx = 5, pady = 5)
+        self.l3 =tk.Label(self.f2, text = 'Flow (gpm)').grid(row = 1, column = 0,  padx = 5, pady = 5)
+        self.l4 =tk.Label(self.f2, text = 'Diam (in)').grid(row = 2, column = 0,  padx = 5, pady = 5)
+        
+        self.ent1 = tk.Entry(self.f2)
+        self.ent1.grid(row = 1, column = 1, padx = 5, pady = 5)
+        
+        self.ent2 = tk.Entry(self.f2)
+        self.ent2.grid(row = 2, column = 1, padx = 5, pady = 5) 
+        
+        self.butt2 = tk.Button(self.f2, text = 'Calculate', command = lambda: self.vel_head())
+        self.butt2.grid(row = 3, column = 0, columnspan = 2, padx = 5, pady = 5)
+        
+        
+    def vel_head(self):
+        """ """
+        e1 = float(self.ent1.get())
+        e2 = float(self.ent2.get())
+        vhead = syshead.velocity_head(Q = e1, d = e2)
+        
+        self.butt2.config(text = 'Next')
+        self.l5 = tk.Label(self.f2, text = 'Velocity Head = {} ft.'.format(vhead)).grid(row = 4, column = 0, columnspan = 2,  padx = 5, pady = 5)
+        
+
+if __name__ == '__main__':
+    root = tk.Tk()
+    Main(root)
+    root.mainloop()
+
