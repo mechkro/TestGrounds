c69cead6c989a0ef9378faa5beb2b53e55d85d50
mechkro <kinsey30@yahoo.com>
2020-04-11 23:49:12 -0700

    Create context_menu.py

---
+++ App Menus/context_menu.py
@@ -0,0 +1,40 @@
+from tkinter import *
+from tkinter import ttk
+
+
+
+root = Tk()
+root.resizable(FALSE, FALSE)
+menu = Menu(root)
+
+p = ttk.Panedwindow(root, orient=VERTICAL)
+# first pane, which would get widgets gridded into it:
+f1 = ttk.Labelframe(p, text='Pane1', width=100, height=50)
+f2 = ttk.Labelframe(p, text='Pane2', width=100, height=50)   # second pane
+p.add(f1)
+p.add(f2)
+p.grid(row = 0, column = 0)
+
+p2 = ttk.Panedwindow(root, orient= HORIZONTAL)
+# first pane, which would get widgets gridded into it:
+f3 = ttk.Labelframe(p2, text='Pane1', width = 50, height=100)
+f4 = ttk.Labelframe(p2, text='Pane2', width= 50, height=100)   # second pane
+p2.add(f3)
+p2.add(f4)
+p2.grid(row = 0, column = 1)
+
+
+label = Label(root, text = 'Right Click GUI').pack(padx = 5, pady = 5)
+lbox = Listbox(root, width = 25, height = 8)
+lbox.pack(padx = 5, pady = 5)
+
+for i in ('Copy', 'Edit', 'Remove', 'Tag', 'Select'):
+    menu.add_command(label=i)
+    
+if (root.tk.call('tk', 'windowingsystem')=='aqua'):
+    root.bind('<2>', lambda e: menu.post(e.x_root, e.y_root))
+    root.bind('<Control-1>', lambda e: menu.post(e.x_root, e.y_root))
+else:
+    root.bind('<3>', lambda e: menu.post(e.x_root, e.y_root))
+root.mainloop()
+
