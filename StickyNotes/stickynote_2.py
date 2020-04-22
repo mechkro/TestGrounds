79e57c68036e446fae780a6aac32a71361c736b4
mechkro <kinsey30@yahoo.com>
2020-04-21 21:51:54 -0700

    Update stickynote_2.py
    Made new entry window more robust and better features:
    Added Inquiry date calendar selection and Due date calendar selection

--- StickyNotes/stickynote_2.py
+++ StickyNotes/stickynote_2.py
@@ -2,7 +2,7 @@ import tkinter as tk
 import collections as clc
 #import tkMessageBox as tkm
 import sqlite3 as sql
-
+from tkcalendar import Calendar, DateEntry
 
 """
 Post-it type reminder board -----------
@@ -22,19 +22,20 @@ CURRENT ISSUES:
     
 """
 
+#COLOR PARAMETERS -----------------------------
 BG = '#0C1021'
 FG = 'white'
-
-
 DGR = 'dark goldenrod'
 BLK = 'black'
 
+
+#-----------------------------------------------------
 class MemoBoard(tk.Tk):
 
     current_pages = 1
     pages = clc.OrderedDict()
     
-    
+    #-----------------------------------------------------
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
         
@@ -44,22 +45,30 @@ class MemoBoard(tk.Tk):
         
         self.start_db_func()
         
+        #Frame Widget ---------------------
         self.frame = tk.Frame(self, bg = BG)
         self.frame.grid()
 
+        #Label Widget ---------------------
         self.flab = tk.Label(self.frame, text = 'Click Button\nto ADD newnote',
                              bg = BG, fg = FG,font = ('Verdana', 12, 'bold'))
         self.flab.grid(row = 0, column = 0, columnspan = 6, padx = 5, pady = 5)
 
+        
+        #Button Widgets ---------------------
         self.leftb = tk.Button(self.frame, text = '<', command = lambda: self.previous_page(), bg = BG, fg = FG)
         self.leftb.grid(row = 1, column = 0, padx = 5, pady = 5)
 
         self.rightb = tk.Button(self.frame, text = '>', command = lambda: self.next_page(), bg = BG, fg = FG)
         self.rightb.grid(row = 1, column = 5, padx = 5, pady = 5)
 
+
+        #Label Frame Widget-------------------
         self.noteframe = tk.LabelFrame(self.frame, text = 'Notes', bg = BG, fg = FG)
         self.noteframe.grid(row = 2, column = 0, columnspan = 6, padx = 5, pady = 5)
 
+
+        #Variable Container -------------------
         self.master_buttmap = {}
         self.buttonmap = {}
         self.nhold = {}
@@ -81,7 +90,7 @@ class MemoBoard(tk.Tk):
             print(k)
             for x,z in v.items():
                 print(x)
-                print(z)
+                print(repr(z))
         
 
         self.fbut = tk.Button(self.frame, text = 'ADD Note', command = lambda: None, bg = BG, fg = FG)
@@ -90,9 +99,11 @@ class MemoBoard(tk.Tk):
         self.wm_protocol("WM_DELETE_WINDOW", self.on_cancel)
 
 
-
+    #-----------------------------------------------------
     def previous_page(self):
-        """ """
+        """ 
+        
+        """
         
         if MemoBoard.current_pages == 1:
             pass
@@ -102,20 +113,29 @@ class MemoBoard(tk.Tk):
         self.load_current_pagemap()
         
 
-
+    #-----------------------------------------------------
     def next_page(self):
-        """ """
+        """ 
+        
+        """
         
         MemoBoard.current_pages += 1
         self.load_current_pagemap()
 
-
+    #-----------------------------------------------------
     def load_current_pagemap(self):
+        """ 
+        
+        """
+        
         print(MemoBoard.current_pages)
         return
 
+    #-----------------------------------------------------
     def add_menu_commands(self):
-        """ """
+        """ 
+        
+        """
         
         #for i in ('Copy', 'Edit', 'Remove', 'Tag', 'Select', 'Cancel'):
         self.menu.add_command(label='Copy', command = lambda: self.mouse_click('Copy'))
@@ -131,7 +151,9 @@ class MemoBoard(tk.Tk):
 
     #----------------------------------------------------
     def mouse_click(self, txt):
-        """ """
+        """ 
+        
+        """
 
         director = {
                 'Copy': self.copy_func,
@@ -147,37 +169,66 @@ class MemoBoard(tk.Tk):
 
     #-----------------------------------------------------
     def copy_func(self):
-        """ """
+        """ 
+        
+        """
+        
         print('copy func called')
         self.menu2 = tk.Menu(self.menu, bg = BG, fg = FG)
         self.menu2.add_command(label='Copy', command = lambda: None)
         self.bind('<3>', lambda e: self.menu2.post(e.x, e.y))
         
+        
     #-----------------------------------------------------
     def edit_func(self):
-        """ """
+        """ 
+        
+        """
+        
         print('edit func called')
+        
+        
     #-----------------------------------------------------
     def remove_func(self):
-        """ """
+        """ 
+        
+        """
+        
         print('remove func called')
+        
+        
     #-----------------------------------------------------
     def tag_func(self):
-        """ """
+        """ 
+        
+        """
+        
         print('tag func called')
+        
+        
     #-----------------------------------------------------
     def select_func(self):
-        """ """
+        """ 
+        
+        """
+        
         print('select func called')
+        
+        
     #-----------------------------------------------------
     def cancel_func(self):
-        """ """
+        """ 
+        
+        """
+        
         print('cancel func called')
 
 
     #-----------------------------------------------------
     def print_text(self):
-        """ """
+        """ 
+        
+        """
         
         print(self.varstr.get())
 
@@ -220,13 +271,13 @@ class MemoBoard(tk.Tk):
         #b.bind('<Leave>', self.leave_widget)
         return b
 
+
     #---------------------------------------------------------------------
     def check_if_txt(self, s):
         """
 
         """
 
-        
         if MemoBoard.pages[s]:
             self.add_memo(s, MemoBoard.pages[s])
 
@@ -242,33 +293,89 @@ class MemoBoard(tk.Tk):
                   - Make button just show X and change color on main page
         """
         
-        self.tlevel = tk.Toplevel()
+        self.tlevel = tk.Toplevel(bg = BG)
+        self.tlevel.title('Add NOTE entry page')
 
-        self.memolab = tk.Label(self.tlevel, text = 'Enter MEMO', fg = 'black', font = ('Verdana', 12, 'bold'))
-        self.memolab.grid(row = 0, padx = 5, pady = 5)
+        #INQUIRY DATE Widgets-------------------------------------------------------------
+        self.memolab = tk.Label(self.tlevel, text = 'Enter MEMO', bg = BG, fg = FG, font = ('Verdana', 16, 'bold'))
+        self.memolab.grid(row = 0, columnspan = 2, padx = 5, pady = 5)
         
-        self.txt = tk.Text(self.tlevel,bg = 'grey', fg = 'black', font = ('Verdana', 12, 'bold'))
-        self.txt.grid(row = 1, padx = 5, pady = 5)
+        self.dateinit_lab = tk.Label(self.tlevel, text = 'Inquiry Date:', bg = BG, fg = FG, font = ('Verdana', 13, 'bold'))
+        self.dateinit_lab.grid(row = 1, column = 0, padx = 5, pady = 5)
+        self.dentry_init = DateEntry(self.tlevel, locale='en_US', date_pattern='MM/dd/yyyy',
+                                background = BG, foreground = FG, headersbackground = BG, headersforeground = DGR,
+                                bordercolor = DGR,normalbackground = BG, normalforeground = FG, 
+                                weekendbackground = BG, weekendforeground = FG,
+                                selectbackground = DGR, selectforeground = 'black',
+                                othermonthforeground = 'dim gray', othermonthbackground = BG, 
+                                othermonthweforeground = 'dim gray', othermonthwebackground = BG, 
+                                disabledforeground='red',tooltipbackground = BG, tooltipforeground = DGR)
+        
+        self.dentry_init.grid(row = 1, column = 1, padx = 5, pady = 5)
+        self.dentry_init.bind('<<DateEntrySelected>>', self.add_inquiry_date)
+        
+        
+        #DUE DATE Widgets-------------------------------------------------------------
+        self.datedue_lab = tk.Label(self.tlevel, text = 'Due Date:', bg = BG, fg = FG, font = ('Verdana', 13, 'bold'))
+        self.datedue_lab.grid(row = 2, column = 0, padx = 5, pady = 5)        
+        self.dentry_due = DateEntry(self.tlevel, locale='en_US', date_pattern='MM/dd/yyyy',
+                                background = BG, foreground = FG, headersbackground = BG, headersforeground = DGR,
+                                bordercolor = DGR,normalbackground = BG, normalforeground = FG, 
+                                weekendbackground = BG, weekendforeground = FG,
+                                selectbackground = DGR, selectforeground = 'black',
+                                othermonthforeground = 'dim gray', othermonthbackground = BG, 
+                                othermonthweforeground = 'dim gray', othermonthwebackground = BG, 
+                                disabledforeground='red',tooltipbackground = BG, tooltipforeground = DGR, state = tk.DISABLED)
+        
+        self.dentry_due.grid(row = 2, column = 1, padx = 5, pady = 5)
+        self.dentry_due.bind('<<DateEntrySelected>>', self.add_due_date)        
+        
+        self.txt = tk.Text(self.tlevel, bg = BG, fg = FG, font = ('Verdana', 12, 'bold'), 
+                           insertbackground = FG, bd = 3, state = tk.NORMAL)
+        self.txt.grid(row = 3, columnspan = 2, padx = 10, pady = 10)
 
         if not txt:
             pass
         else:
             self.txt.insert(tk.END, txt)
         
-        self.entb = tk.Button(self.tlevel, text = 'Submit', bg = 'grey', fg = 'black', font = ('Verdana', 12, 'bold'),
+        self.entb = tk.Button(self.tlevel, text = 'Submit', bg = BG, fg = FG, font = ('Verdana', 12, 'bold'),
                               command = lambda: self.grab_memo(self.txt.get(1.0, tk.END), s))
-        self.entb.grid(row = 2, padx = 5, pady = 5, sticky = tk.EW)
+        self.entb.grid(row = 4, columnspan = 2, padx = 5, pady = 5, sticky = tk.EW)
 
-        for i in self.buttonmap.values():
-            print(i[-1].cget('text'))
+        #for i in self.buttonmap.values():
+            #print(i[-1].cget('text'))
 
         self.tlevel.protocol("WM_DELETE_WINDOW", lambda: self.tlevel.destroy())
         self.tlevel.mainloop()
 
 
     #---------------------------------------------------
+    def add_inquiry_date(self, event):
+        """ 
+        
+        """
+        self.dentry_due.config(state = tk.NORMAL)
+        self.txt.insert(tk.END, 'Inquiry Date: {}\n'.format(event.widget.get_date()))
+        return
+        
+    #---------------------------------------------------
+    def add_due_date(self, event):
+        """ 
+        
+        """
+        self.txt.insert(tk.END, 'Inquiry Date: {}\n'.format(event.widget.get_date()))
+        self.txt.insert(tk.END, '----------------------------------------------------------------------\n')
+        self.txt.config(state = tk.NORMAL)
+        return        
+
+
+    #---------------------------------------------------
     def grab_memo(self, txt, w):
-        """ """
+        """ 
+        
+        """
+        
         holdr = txt     #NEED to have this stored in db. where we can call on it instead of making actual dislay main show it
         date = '2020-04-17'
         c = self.c.cursor()
@@ -287,6 +394,10 @@ class MemoBoard(tk.Tk):
 
     #----------------------------------------------------
     def print_db_content(self):
+        """ 
+        
+        """
+        
         c = self.c.cursor()
         c.execute("""SELECT * FROM test""")
         for i in c.fetchall():
@@ -295,16 +406,22 @@ class MemoBoard(tk.Tk):
         c.close()
         print('Succesful DB Select')
 
+
     #-------------------------------------------
     def ignore(self):
-        """ """
+        """ 
+        
+        """
         
         pass
 
     
     #-----------------------------------------
     def check_if_color(self):
-        """ """
+        """ 
+        
+        """
+        
         
         for b in self.nhold.values():
             pass            
@@ -323,7 +440,9 @@ class MemoBoard(tk.Tk):
 
     #--------------------------------------
     def leave_widget(self, event):
-        """ """
+        """ 
+        
+        """
         
         event.widget.config(bg = 'grey', fg = 'black', text = '-')
         
@@ -332,7 +451,9 @@ class MemoBoard(tk.Tk):
 
     #-------------------------------------------------------
     def pop_up_details(self, winfoloc):
-        """ """
+        """ 
+        
+        """
         
         pass
 
