c1a577a0c1c73471c9844d3879c76faa75e00203
mechkro <kinsey30@yahoo.com>
2020-10-06 17:56:04 -0700

    Update dbtest2.py

--- db/dbtest2.py
+++ db/dbtest2.py
@@ -3,7 +3,8 @@ import sqlite3 as sql3
 import random as rand
 import calendar as cal
 import collections as clc
-
+import json
+import datetime as dt
 
 opts = """Ign:1 https://storage.googleapis.com/cros-packages/85 buster InRelease
 Get:2 https://storage.googleapis.com/cros-packages/85 buster Release [3,119 B]
@@ -22,17 +23,19 @@ Reading state information... Done
 36 packages can be upgraded. Run 'apt list --upgradable' to see them.
 N: Repository 'https://deb.debian.org/debian buster InRelease' changed its 'Version' value from '10.4' to '10.6'""".splitlines()
 
+#---------------------------------------
 class main:
     
     #--------------------------------------
     def __init__(self, parent):
         """ 
-        
+        calls all the functions in order necc. to build out GUI
         """
         
         self.parent = parent
         self.parent.title('Main DB')
         
+        self.entriestracker = {}
         self.create_widgets()
         self.load_db()
         self.pull_db_info()
@@ -79,6 +82,8 @@ class main:
         
         self.lbox = tk.Listbox(self.parent, width = w, height = h)
         self.lbox.grid()
+        self.lbox.bind('<Button-1>', self.alterlabel)
+        self.lbox.bind('<Double-Button-1>', self.newtoplevel)
     
     
     #DATABASE SECTION -----------------------------------------
@@ -114,7 +119,11 @@ class main:
     #--------------------------------------
     def insert_db(self):
         """ 
-        
+        insert and update will work together.
+        - first we must make a copy of the current note in db. 
+        - then append to the end of the note
+        - re-submit or update the value
+        - ***Only when editing will we not need a copy
         """
         
         pass
@@ -130,37 +139,132 @@ class main:
     #--------------------------------------
     def pull_db_info(self):
         """ 
-        
+        call to calendar function for given year to return datetime objects
         """
         self.dataents = self.pullcalldates()
+        return
         
     #--------------------------------------
     def populate_list(self):
         """ 
         
         """
-        
-        for i in range(365):
-            randval = rand.choice(opts)
-            self.lbox.insert(tk.END, f"{i} - {randval}")
+        for k,v in self.dataents.items():
+            yr  = str(v).split('-')[0]
+            if yr == '2020':
+                randval = rand.choice(opts)
+                self.lbox.insert(tk.END, f"{v} : Entries = None ")
+                self.entriestracker[v] = 0
+            else:
+                pass
     
     #--------------------------------------
     def pullcalldates(self):
         """ 
-        
+        grabs all the datetime dates from calendar module to be used as ref for 
+        database writing of notes.
         """
         datelist = cal.Calendar()
-        dlist = datelist.yeardatescalendar()
+        dlist = datelist.yeardatescalendar(2020)
         itr = 0
         self.d = {}
-        for i in y[:]:
+        for i in dlist[:]:
             for j in i:
                 for k in j:
                     for u in k:
                         itr += 1
                         self.d[itr] = u
         return self.d
+    
+    #--------------------------------------
+    def serializedata(self, entdate, dictobj):
+        """ 
+        entdate = the date which will have contents updated
+        dictobj = dict object that will be passed to json.dumps to have a string object
+        and loaded to the db file. 
+        When we need to retrieve the string object will be sent thru json.loads to produce
+        a DICTIONARY OBJECT AGAIN
         
+        json example:
+        k = {'2020-10-12':'Test Note 1', 'accnt 1':'note1', 'accnt 2':'note2'}
+        y = json.dumps(k)  -- string object
+        x = json.loads(y)  -- dict object
+        """
+        
+        dict_length = len(dictobj.values())
+        jsonobj = json.dumps(dictobj)
+        self.con.execute("""UPDATE table SET notes = ?, qtyents = ? WHERE date = ?""",(jsonobj, dict_length,entdate))
+        self.con.commit()
+    
+    
+    #EVENT BINDING FUNCTIONS ----------------------------------------------------
+    #--------------------------------------
+    def alterlabel(self,event):
+        """ 
+        """
+        
+        curselect = event.widget.get(tk.ACTIVE).split(':')
+        self.lab.config(text = f"Current Date: {curselect[0]}")
+        return
+
+    #--------------------------------------
+    def newtoplevel(self, event):
+        """
+        """
+        
+        self.dbtop = tk.Toplevel()
+        curselect = event.widget.get(tk.ACTIVE).split(':')
+        self.toplab = tk.Label(self.dbtop, text = curselect[0])
+        self.toplab.grid(row = 0, column = 0, columnspan = 2)
+        self.txtbox = tk.Text(self.dbtop)
+        self.txtbox.grid(row = 1, column = 0, columnspan = 2)
+        self.repbutt = tk.Button(self.dbtop, text = 'Replace Entry', command = lambda: self.newentryparser(curselect[0]))
+        self.repbutt.grid(row = 2, column = 0, sticky = tk.EW)
+        self.addbutt = tk.Button(self.dbtop, text = 'Add Entry', command = lambda: None)
+        self.addbutt.grid(row = 2, column = 1, sticky = tk.EW)
+        self.sepbutt = tk.Button(self.dbtop, text = 'Add Entry', command = lambda: self.addseperator())
+        self.sepbutt.grid(row = 3, column = 0, columnspan = 2)
+        self.dbtop.mainloop()
+
+    #--------------------------------------
+    def addseperator(self):
+        """
+        This is assuming going forward teh seperator between entries which will be split is ------
+        The entire entry treated as a string and each entry is distinguishable by that dash -----
+        """
+        
+        tod = dt.datetime.today()           #Add a timestamp to the entry
+        seper = f"{tod}:\n-----\n"
+        self.txtbox.insert(tk.END, seper)
+        return
+
+
+    #--------------------------------------
+    def newentryparser(self, d):
+        """
+        pull the entire field of the text box as a string and parse it by a defined splitter (-----)
+        after put each of the entries in a dict so that we can use json method to pass to db
+        """
+        
+        jsdict = {}
+        enttxt = self.txtbox.get(1.0, tk.END).split('-----')
+        for n,i in enumerate(enttxt):
+            jsdict[n] = i.strip('\n')
+        
+        self.entriestracker[d] = len(jsdict.values())       #Keeping track of entry totals for each date to display on the main listbox
+        for k,v in self.entriestracker.items():
+            print(k,v)
+            
+        print(f'Total Entries : {len(jsdict.values())}')
+        print(jsdict.items())
+        go = json.dumps(jsdict)
+        pull = json.loads(go)
+        print(go)
+        print(pull)
+        
+        
+
+
 
 #MAIN----------------------------
 #--------------------------------
@@ -168,6 +272,3 @@ if __name__ == '__main__':
     root = tk.Tk()
     main(root)
     root.mainloop()
-        
-    
-        
